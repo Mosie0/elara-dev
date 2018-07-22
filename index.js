06954a603d3e59db7d75b4f1a73cdcf0dd0131c6
SUPERCHIEFYT <34470712+SUPERCHIEFYT@users.noreply.github.com>
2018-07-22 11:10:17 -0700

    new

--- index.js
+++ index.js
@@ -162,21 +162,7 @@ fs.readdir("./commands/BotOwner", (err, files) => {
         bot.commands.set(props.help.name, props);
     });
 });
-fs.readdir("./commands/BotOwner/BotInfos", (err, files) => {
 
-    if (err) console.log(err);
-    let jsfile = files.filter(f => f.split(".").pop() === "js");
-    if (jsfile.length <= 0) {
-        console.log("Couldn't find Bot Ower/BotInfos commands.");
-        return;
-    }
-
-    jsfile.forEach((f, i) => {
-        let props = require(`./commands/BotOwner/BotInfos/${f}`);
-        console.log(`${f} loaded!`);
-        bot.commands.set(props.help.name, props);
-    });
-});
 fs.readdir("./commands/BotOwner/RoleInfo", (err, files) => {
 
     if (err) console.log(err);
