918212f61576bfd2c45446752a48a0f8623a2d09
SUPERCHIEFYT <34470712+SUPERCHIEFYT@users.noreply.github.com>
2018-07-22 13:17:15 -0700

    new

--- commands/BotOwner/MyServerCrap/SUPERRULES.js
+++ commands/BotOwner/MyServerCrap/SUPERRULES.js
@@ -7,7 +7,7 @@ module.exports.run = async (bot, message, args) => {
         .setDescription("The Rules Of SUPERCHIEFYT's Discord")
         .setColor("#000FF")
         .setThumbnail(bicon)
-        .addField("General Rules", "1. Do not agrue with the <@&455152231956873236>\n2. No cursing in the discord incuding Voice chat's\n3. Listen to and work with staffs, not against them.\n4. No text spamming and cussing in the text channels\n5. No mic spamming and cussing in the voice channels.\n6. Trolling and bullying is not allowed in any form.\n8. No self bots")
+        .addField("General Rules", "1. Do not agrue with the <@&455152231956873236>\n2. No cursing in the discord incuding Voice chat's\n3. Listen to and work with staffs, not against them.\n4. No text spamming and cussing in the text channels\n5. No mic spamming and cussing in the voice channels.\n6. Trolling and bullying is not allowed in any form.\n7. No self bots")
         .addBlankField()
         .addField("AutoBan", "1. Not listening to staff after being warned Alot\n2. Pornographic, disturbing content, or racism.\n3. Links to Porn or Racism Sites disturbing stuff\n4. Impersonate another person or Bot.\n5. Snipping In the Discord\n6. No Drama From Other Servers, after being told to stop and not doing so.")
         .addBlankField()
