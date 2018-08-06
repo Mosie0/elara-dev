e78fc9dcbe2c092769e08be8a7763e106d464882
SUPERCHIEFYT <34470712+SUPERCHIEFYT@users.noreply.github.com>
2018-08-05 17:59:44 -0700

    new

--- commands/Moderation/discordupdates.js
+++ commands/Moderation/discordupdates.js
@@ -1,15 +1,16 @@
 const Discord = require('discord.js');
 module.exports.run = async (bot, message, args) => {
-    if(!message.member.hasPermission("MANAGE_SERVER")) return;
-let announce = message.guild.channels.find('name', 'discord-updates');
-let botembed = new Discord.RichEmbed()
-.setColor(`#FF000`)
-.setDescription(args.join(' '))
-.setAuthor(`${message.guild.name}`, `${message.guild.iconURL}`)
-.setFooter(`Announcement From ${message.author.username}`, message.author.avatarURL)
-announce.send(botembed);
-message.delete().catch();
+    if (!message.member.hasPermission("MANAGE_SERVER")) return;
+    let announce = message.guild.channels.find('name', 'discord-updates');
+    let botembed = new Discord.RichEmbed()
+        .setColor(`#FF000`)
+        .setTitle(`New ${args[0]} Update!`)
+        .setDescription(args.join(' ').slice(args[0]))
+        .setAuthor(`${message.guild.name}`, `${message.guild.iconURL}`)
+        .setFooter(`Announcement From ${message.author.username}`, message.author.avatarURL)
+    announce.send(botembed);
+    message.delete().catch();
 }
-module.exports.help = { 
+module.exports.help = {
     name: "change"
 }
