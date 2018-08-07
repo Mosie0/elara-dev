5fe90d4538162493c8b3c94be8effc1008ef7481
SUPERCHIEFYT <34470712+SUPERCHIEFYT@users.noreply.github.com>
2018-08-06 21:24:14 -0700

    new

--- commands/BotOwner/OwnerCommands/restart.js
+++ commands/BotOwner/OwnerCommands/restart.js
@@ -2,6 +2,8 @@ const Discord = require("discord.js");
 const fs = require('fs');
 
 module.exports.run = async (bot, message, args) => {
+    let modlogs = bot.channels.get('468372950266150916')
+    if(!modlogs) return;
     let botembed = new Discord.RichEmbed()
     .setColor("#000FF")
     .addField("Restart Issued", `<@${message.author.id}> Has Restarted the Bot!`)
@@ -12,12 +14,10 @@ module.exports.run = async (bot, message, args) => {
         .setColor("#FF0000")
         .setDescription(`Can't Find the **modlogs** Channel to Post my Restarting Message! **${bot.user.username}** Can't be Restart without that Channel!`)
         .setFooter("Command Ran By: " + message.author.username, message.author.avatarURL);
-    let alert = message.guild.channels.find('name', "modlogs");
-    if (!alert) return message.channel.send(cantfindembed);
     console.log(`Bot Has Been Restart By: ` + message.author.username);
     if (message.author.id !== "288450828837322764") return message.reply("Sorry But this is a Bot Owner Only Command.");
     await message.react("✅");
-    await alert.send(botembed);
+    await modlogs.send(botembed);
     message.delete(1000).catch();
     fs.writeFile('./log.json', JSON.stringify(`Bot has Been Restarted.`), (err) => {
         if (err) console.log(err)
