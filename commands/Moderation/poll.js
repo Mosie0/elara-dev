9f2801a095ad4ec8fe49d31128c49b0ada312151
SUPERCHIEFYT <34470712+SUPERCHIEFYT@users.noreply.github.com>
2018-08-25 06:02:29 -0700

    new

--- commands/Moderation/poll.js
+++ commands/Moderation/poll.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 module.exports.run = async (bot, message, args) => {
-    if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send(`Sorry But you need the Manage Messages Permission.`)
+    if (!message.member.hasPermission('MANAGE_GUILD')) return message.channel.send(`Sorry But you need the Manage Messages Permission.`)
     let suggestionchannel = message.mentions.channels.first()
     if (!args.slice(1).join(' ')) return message.channel.send(`Please Provide Something for the Poll!`)
     message.delete().catch()
