78876df5f7ff0356480277fd1d6b0e58b2a423e4
SUPERCHIEFYT <34470712+SUPERCHIEFYT@users.noreply.github.com>
2018-08-25 07:02:08 -0700

    new

---
+++ commands/BotOwner/OwnerCommands/discordbotslookup.js
@@ -0,0 +1,33 @@
+const snek = require('node-fetch'),
+    moment = require('moment'),
+    { MessageEmbed } = require('discord.js');
+module.exports.run = async (bot, msg, args) => {
+    try {
+        const res = await snek(`https://discordbots.org/api/bots/${bot}`, { headers: { Authorization: process.env.DISCORDBOTLIST } }),
+            info = await res.json(),
+            infoEmbed = new MessageEmbed();
+
+        infoEmbed
+            .setColor(msg.guild ? msg.guild.me.displayHexColor : '#7CFC00')
+            .setTitle(`Discord Bots Info for ${info.username}#${info.discriminator} (${info.client.id})`)
+            .setURL(`https://discordbots.org/bot/${info.client.id}`)
+            .setThumbnail(`https://images.discordapp.net/avatars/${info.client.id}/${info.avatar}.png`)
+            .setDescription(info.shortdesc)
+            .setFooter(`${info.username}#${info.discriminator} was submitted`)
+            .setTimestamp(moment(info.date)._d)
+            .addField('Default Prefix', info.prefix, true)
+            .addField('Library', info.lib, true)
+            .addField('Server Count', info.server_count, true) // eslint-disable-line camelcase
+            .addField('Shards Count', info.shards.length, true)
+            .addField('Invite Link', `[Click Here](${info.invite})`);
+
+        return msg.embed(infoEmbed, `https://discordbots.org/bot/${info.client.id}`);
+    } catch (err) {
+        console.log(err)
+        return msg.reply('an error occurred getting info from that bot, are you sure it exists on the website?');
+    }
+
+}
+module.exports.help = {
+    name: "botlookup"
+}
