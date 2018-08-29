ef5f7a69ba7600ebed3b6f36ecdbc1a53b9a67e1
SUPERCHIEFYT <34470712+SUPERCHIEFYT@users.noreply.github.com>
2018-08-29 00:20:54 -0700

    new

--- playing.js
+++ playing.js
@@ -1,29 +1,22 @@
 module.exports = (bot) => {
 	bot.user.setActivity(`Prefix e! or E!`, { type: "STREAMING", url: "https://www.twitch.tv/elarabots_discord" });
+	let userssize = bot.users.size;
+	let channelsize = bot.channels.size;
+	let guildsize = bot.guilds.size;
 	var status = [
-		`Serving: ${bot.guilds.size} Servers, ${bot.channels.size} Channels, ${bot.users.size} Users`,
-		`More Updates Coming Soon!`,
-		`My Support Server: https://discord.gg/hgsM86w`
+		`Prefix e! or E! | Serving: ${guildsize} Servers, ${channelsize} Channels, ${userssize} Users`,
+		`Prefix e! or E! | More Updates Coming Soon!`,
+		`Prefix e! or E! | My Support Server: https://discord.gg/hgsM86w`
 	];
 	var types = [
-		`STREAMING`,
-		`STREAMING`,
-		`STREAMING`,
-		`STREAMING`,
-		`STREAMING`,
-		`STREAMING`,
 		`STREAMING`,
 		`STREAMING`,
 		`STREAMING`
 	];
 	gameval = 0;
 	setInterval(() => {
-		if (bot.user.presence.game.name.startsWith(`Prefix e! or E!`)) {
-			if (gameval == status.length) {
-				gameval = 0;
-			}
-			bot.user.setActivity(`Prefix e! or E! | ${status[gameval]}`, { type: types[gameval], url: "https://www.twitch.tv/elarabots_discord" });
-			gameval++;
-		}
+		if (gameval == status.length) { gameval = 0; }
+		bot.user.setActivity(`${status[gameval]}`, { type: types[gameval], url: "https://www.twitch.tv/elarabots_discord" });
+		gameval++;
 	}, 10000);
 };
