238a86c874c27c3a14aac93f71e08058199d52ce
SUPERCHIEFYT <34470712+SUPERCHIEFYT@users.noreply.github.com>
2018-08-29 00:53:56 -0700

    new

--- playing.js
+++ playing.js
@@ -11,6 +11,5 @@ module.exports = (bot) => {
 	setInterval(() => {
 		let gameval = Math.floor((Math.random() * status.length));
 		bot.user.setActivity(`${status[gameval]}`, { type: "STREAMING", url: "https://www.twitch.tv/elarabots_discord" });
-		gameval++;
 	}, 10000);
 };
