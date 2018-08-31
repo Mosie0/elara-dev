d3985707187495c2b6eb6c808284cae0c00bc411
SUPERCHIEFYT <34470712+SUPERCHIEFYT@users.noreply.github.com>
2018-08-31 06:39:42 -0700

    new

--- commands/Info/roles.js
+++ commands/Info/roles.js
@@ -1,29 +1,30 @@
 const Discord = require("discord.js");
+const hastebin = require('hastebin-gen');
 module.exports.run = async (bot, msg, args) => {
     if (msg.guild.roles.size === 0) {
         return msg.channel.send('There are no roles on this server.');
     }
     else if (msg.guild.roles.sort((b, a) => { return a.position - b.position }).map(role => `<@&${role.id}>`).join("\n").length > 2000) {
         let rolenumber = msg.guild.roles.size;
-        let s = msg.guild.roles.sort((b, a) => { return a.position - b.position }).map(role => `<@&${role.id}>`).join("\n")
+        let s = msg.guild.roles.sort((b, a) => { return a.position - b.position }).map(role => `Mention: <@&${role.id}>, Name <${role.name}> ID: <${role.id}>`).join("\n")
         let i = Math.ceil(s.length / 2);
         let partOne = s.slice(0, i).trim();
         let partTwo = s.slice(i + 1, s.length).trim();
-        let embed1 = new Discord.RichEmbed()
-            .setColor(`#FF000`)
-            .setTitle(`Current Server Roles [${rolenumber}]`)
-            .setDescription(`${partOne}`)
-        msg.channel.send(embed1);
-        let embed2 = new Discord.RichEmbed()
-            .setColor(`#FF000`)
-            .setDescription(`${partTwo}`)
-        msg.channel.send(embed2);
+        hastebin(partOne + partTwo, "js").then(r => {
+            var hastLink = r
+            const hastEmb = new Discord.RichEmbed()
+                .setColor(`RANDOM`)
+                .setTitle(`To Many Roles in the Server here is a Link to see all of the roles\nCurrently [${rolenumber}] Roles in the Server`)
+                .addField('Link: ', `${hastLink}`)
+            msg.channel.send({ embed: hastEmb })
+        }).catch(console.error);
     }
     else {
+        let rolenumber = msg.guild.roles.size;
         let embed = new Discord.RichEmbed()
             .setColor(`#FF000`)
-            .setTitle(`Current Server Roles`)
-            .setDescription(`${msg.guild.roles.map(role => role).join('|')}`)
+            .setTitle(`Current Server Roles [${rolenumber}]`)
+            .setDescription(`${msg.guild.roles.sort((b, a) => { return a.position - b.position }).map(role => `${role}`).join(" | ")}`)
         msg.channel.send(embed);
     }
 }
