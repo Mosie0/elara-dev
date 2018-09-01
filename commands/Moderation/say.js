186a9a462e69475f6279397db1fb174182c02610
SUPERCHIEFYT <34470712+SUPERCHIEFYT@users.noreply.github.com>
2018-08-31 17:18:42 -0700

    new

--- commands/Moderation/say.js
+++ commands/Moderation/say.js
@@ -1,10 +1,10 @@
-const Discord = require("discord.js");
-
 module.exports.run = async (bot, message, args) => {
     if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Sorry but you don't have the permissions to use this command.");
-    let botmessage = args.join(" ");
+    let channels = message.mentions.channels.first() || message.guild.channels.get(args[0])
+    if (!channels) channels = message.channel.send(args.join(' '))
+    let botmessage = args.slice(1).join(" ");
     message.delete().catch();
-    message.channel.send(botmessage);
+    channels.send(botmessage);
 }
 
 
