3804c922e6a7d2d7f08d4a45b4239e674d13d5da
Mo_sie <39349787+Mosie0@users.noreply.github.com>
2018-11-19 13:12:46 -0800

    o.o

--- commands/Info/ranks.js
+++ commands/Info/ranks.js
@@ -3,7 +3,7 @@ module.exports.run = async (bot, message, args) => {
     if (message.author.id !== "283311727477784576") return message.reply("Sorry But this is a Bot Owner Only Command.");
     let ranks1 = new Discord.RichEmbed()
     .setColor("#1bf7f3")
-    .setTitle("Jonbn123's Discord")
+    .setTitle("Lumberjacks")
     .setDescription (`Default rank for all members of the Fan Group.`)
     
     let ranks2 = new Discord.RichEmbed()
