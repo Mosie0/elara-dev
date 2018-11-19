7162c3dfd620e4bf05eb0b3c82960b4bcf24708a
Mo_sie <39349787+Mosie0@users.noreply.github.com>
2018-11-19 12:39:33 -0800

    removed a folder

--- Elara.js
+++ Elara.js
@@ -459,21 +459,5 @@ fs.readdir("./commands/BotOwner/OwnerCommands", (err, files) => {
         bot.commands.set(props.help.names, props);
     });
 });
-fs.readdir("./commands/BotOwner/MyServerCrap", (err, files) => {
-
-    if (err) console.log(err);
-    let jsfile = files.filter(f => f.split(".").pop() === "js");
-    if (jsfile.length <= 0) {
-        console.log("Couldn't find BotOwner/MyServerCrap commands.");
-        return;
-    }
-
-    jsfile.forEach((f, i) => {
-        let props = require(`./commands/BotOwner/MyServerCrap/${f}`);
-        console.log(`${f} loaded!`);
-        bot.commands.set(props.help.name, props);
-        bot.commands.set(props.help.names, props);
-    });
-});
 // End of Getting Commands.
 //=============================================================================================================================================================================================
